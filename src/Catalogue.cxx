8c8a9395a000326540ecaf337a6c98a3a3abba8f
Luke Rasmussen <luke.rasmussen@gmail.com>
2017-04-07 14:45:47 -0500

    Missed SAS and Stata in catalogue with last merge

--- src/Catalogue.cxx
+++ src/Catalogue.cxx
@@ -172,6 +172,7 @@ int Scintilla_LinkLexers() {
 	LINK_LEXER(lmRegistry);
 	LINK_LEXER(lmRuby);
 	LINK_LEXER(lmRust);
+	LINK_LEXER(lmSAS);
 	LINK_LEXER(lmScriptol);
 	LINK_LEXER(lmSmalltalk);
 	LINK_LEXER(lmSML);
@@ -180,6 +181,7 @@ int Scintilla_LinkLexers() {
 	LINK_LEXER(lmSpice);
 	LINK_LEXER(lmSQL);
 	LINK_LEXER(lmSrec);
+	LINK_LEXER(lmStata);
 	LINK_LEXER(lmSTTXT);
 	LINK_LEXER(lmTACL);
 	LINK_LEXER(lmTADS3);
